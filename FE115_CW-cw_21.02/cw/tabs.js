4f126124d0d3ed6b789414155957d0fc617c26ad
Halkevich <96432570+Halkevich@users.noreply.github.com>
2022-02-24 17:38:12 +0300

    end

--- FE115_CW-cw_21.02/cw/tabs.js
+++ FE115_CW-cw_21.02/cw/tabs.js
@@ -1,50 +1,20 @@
-const tabs = function (mainSelector){
-    const tabsContainer = document.querySelector(mainSelector);
 
-    let tabs = tabsContainer.querySelector('.tabs').children;
-    if(tabs.length === 0) return;
+let links = document.getElementsByClassName('tabs');
+for (let i = 0; i < links.length; i++) {
+   links[i].onmouseover = function(){
+       let tooltip = document.getElementsByClassName('tooltip')
+       let too = tooltip[i];
+       too.style.opacity = "1";
+       too.style.visibility = "visible";   
+       console.log(too)
+  }  
 
-    let tabContent = tabsContainer.querySelector('.tab-content').children;
-    if(tabContent.length === 0 || tabContent.length !== tabs.length) return;
-
-
-    [...tabs].map((elem, i) => {
-       elem.addEventListener('click', function (){
-           show(i);
-       })
-    });
-
-    function show(i){
-        [...tabs].map((elem, i) => {
-            elem.classList.remove('active');
-            tabContent[i].classList.remove('active');
-        })
-        tabs[i].classList.add('active');
-        tabContent[i].classList.add('active');
-    }
+  
+  links[i].onmouseout = function(){
+    let tooltip = document.getElementsByClassName('tooltip')
+    let too = tooltip[i];
+    too.style.opacity = "0";
+    too.style.visibility = "hidden";   
+    console.log(too)
+}  
 }
-
-tabs('.tabs-conteiner')
-
-function tooltip() {
-    let elements = document.querySelectorAll('.tooltip');
-
-    [...elements].map(element => {
-        element.addEventListener('mouseenter', function (e) {
-            createTooltip(e)
-        })
-    })
-
-
-    function createTooltip(e) {
-        console.log(e.target)
-
-        let elem = document.createElement('div');
-        elem.setAttribute('class', 'tooltip-element');
-        elem.textContent = e.target.title
-
-        document.body.append(elem);
-    }
-}
-
-tooltip()
