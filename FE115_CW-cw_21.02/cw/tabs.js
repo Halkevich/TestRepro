f8dfd1cf0f3811a62b45c3239cfed6d3543b262f
Halkevich <96432570+Halkevich@users.noreply.github.com>
2022-02-28 17:39:13 +0300

    task-auto

--- FE115_CW-cw_21.02/cw/tabs.js
+++ FE115_CW-cw_21.02/cw/tabs.js
@@ -8,13 +8,12 @@ for (let i = 0; i < links.length; i++) {
        too.style.visibility = "visible";   
        console.log(too)
   }  
-
-  
   links[i].onmouseout = function(){
     let tooltip = document.getElementsByClassName('tooltip')
     let too = tooltip[i];
     too.style.opacity = "0";
     too.style.visibility = "hidden";   
     console.log(too)
-}  
+} 
+  
 }
